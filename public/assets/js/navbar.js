b0c7bf59b6e09ff93b12d670db7fd330bb095e62
Neptune <karl.davalleclercq@ynov.com>
2025-02-21 16:57:38 +0100

    Homepage

--- public/assets/js/navbar.js
+++ public/assets/js/navbar.js
@@ -1,18 +1,20 @@
 categories = document.querySelectorAll(".navbarCategorie")
 navbar = document.querySelector(".navbarMenu")
+// searchIcon = document.querySelector(".")
 
 dev = false
 
 categories.forEach(cat => {
-    cat.addEventListener("click", function(e){
-        console.log(dev)
-        if (dev == true){
-            navbar.classList.remove("navbarDevelopp")
-            
-            dev=false
-        }else{
+    cat.addEventListener("mouseenter", function(e){
             navbar.classList.add("navbarDevelopp")
-            dev=true
-        }
+            cat.classList.add("categorieUnderline")
+    })
+
+    navbar.addEventListener("mouseleave", function(e){
+            navbar.classList.remove("navbarDevelopp")
+            cat.classList.remove("categorieUnderline")
     })
 });
+
+
+// search
