b9e1fcb026ed9632e30f34eb6ac7e12e50359c8f
LucasVellinPatche <124413478+Lsmile974@users.noreply.github.com>
2025-02-21 16:01:15 +0100

    test1

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,73 +3,38 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
-use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-class User implements UserInterface
+#[ORM\Table(name: '`user`')]
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $username = null;
-
-    #[ORM\Column(length: 255, unique: true)]
+    #[ORM\Column(length: 180)]
     private ?string $email = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $password = null;
-
-    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 2)]
-    private ?float $balance = null;
-
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $pictureProfile = null;
-
-    #[ORM\Column(type: Types::JSON)]
+    /**
+     * @var list<string> The user roles
+     */
+    #[ORM\Column]
     private array $roles = [];
 
-    #[ORM\OneToMany(targetEntity: Invoice::class, mappedBy: 'user')]
-    private Collection $invoices;
-
-    #[ORM\ManyToMany(targetEntity: Article::class, mappedBy: 'liked')]
-    private Collection $liked;
-
-    #[ORM\Column(nullable: true)]
-    private ?int $phoneNumber = null;
-
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $deliveryAddress = null;
-
-    #[ORM\OneToOne(mappedBy: 'user', cascade: ['persist', 'remove'])]
-    private ?Cart $cart = null;
-
-    public function __construct()
-    {
-        $this->invoices = new ArrayCollection();
-        $this->liked = new ArrayCollection();
-    }
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    private ?string $password = null;
 
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getUsername(): ?string
-    {
-        return $this->username;
-    }
-
-    public function setUsername(string $username): static
-    {
-        $this->username = $username;
-        return $this;
     }
 
     public function getEmail(): ?string
@@ -80,9 +45,47 @@ class User implements UserInterface
     public function setEmail(string $email): static
     {
         $this->email = $email;
+
         return $this;
     }
 
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     *
+     * @return list<string>
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    /**
+     * @param list<string> $roles
+     */
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
     public function getPassword(): ?string
     {
         return $this->password;
@@ -91,95 +94,16 @@ class User implements UserInterface
     public function setPassword(string $password): static
     {
         $this->password = $password;
+
         return $this;
     }
 
-    public function getBalance(): ?float
-    {
-        return $this->balance;
-    }
-
-    public function setBalance(float $balance): static
-    {
-        $this->balance = $balance;
-        return $this;
-    }
-
-    public function getPictureProfile(): ?string
-    {
-        return $this->pictureProfile;
-    }
-
-    public function setPictureProfile(?string $pictureProfile): static
-    {
-        $this->pictureProfile = $pictureProfile;
-        return $this;
-    }
-
-    public function getRoles(): array
-    {
-        return $this->roles;
-    }
-
-    public function setRoles(array $roles): static
-    {
-        $this->roles = $roles;
-        return $this;
-    }
-
-    public function getInvoices(): Collection
-    {
-        return $this->invoices;
-    }
-
-    public function getLiked(): Collection
-    {
-        return $this->liked;
-    }
-
-    public function getPhoneNumber(): ?int
-    {
-        return $this->phoneNumber;
-    }
-
-    public function setPhoneNumber(?int $phoneNumber): static
-    {
-        $this->phoneNumber = $phoneNumber;
-        return $this;
-    }
-
-    public function getDeliveryAddress(): ?string
-    {
-        return $this->deliveryAddress;
-    }
-
-    public function setDeliveryAddress(?string $deliveryAddress): static
-    {
-        $this->deliveryAddress = $deliveryAddress;
-        return $this;
-    }
-
-    public function getCart(): ?Cart
-    {
-        return $this->cart;
-    }
-
-    public function setCart(?Cart $cart): static
-    {
-        if ($cart && $cart->getUser() !== $this) {
-            $cart->setUser($this);
-        }
-        $this->cart = $cart;
-        return $this;
-    }
-
+    /**
+     * @see UserInterface
+     */
     public function eraseCredentials(): void
     {
-        // Implement this method if storing sensitive data in the entity
-    }
-
-    public function getUserIdentifier(): string
-    {
-        return $this->email;
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 }
