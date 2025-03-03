fbf7ae484731d1478fdf814e35150ec8a06c73d0
ot664vts <luna.colombanfernandez@ynov.com>
2025-03-04 00:42:55 +0100

    merge car pb de connexion

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -1,30 +1,37 @@
 <?php
 
+
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\LoginType;
 use App\Form\RegistrationType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'app_login', methods: ['GET', 'POST'])]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(Request $request, AuthenticationUtils $authenticationUtils): Response
     {
         if ($this->getUser()) {
             return $this->redirectToRoute('homepage');
         }
 
+        // Créer le formulaire de connexion
+        $form = $this->createForm(LoginType::class);
+
+        // Gérer les erreurs de connexion si présentes
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', [
+            'loginForm' => $form->createView(),
             'last_username' => $lastUsername,
             'error' => $error,
         ]);
@@ -43,6 +50,7 @@ class SecurityController extends AbstractController
             $user->setPictureProfile('default.jpg');
             $user->setRoles(['ROLE_USER']);
 
+            // Hash the user's password
             $user->setPassword(
                 $userPasswordHasher->hashPassword(
                     $user,
@@ -66,4 +74,4 @@ class SecurityController extends AbstractController
     {
         // Le logout est géré automatiquement par Symfony
     }
-} 
+}
