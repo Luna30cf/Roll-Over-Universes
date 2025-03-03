d459b1ec19cf449a3b69379163ed0695c7131de1
Neptune <karl.davalleclercq@ynov.com>
2025-03-03 23:56:34 +0100

    panier + log perma

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -12,6 +12,7 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
+
 class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'app_login', methods: ['GET', 'POST'])]
