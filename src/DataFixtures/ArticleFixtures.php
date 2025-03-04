b69cf85e18967f3b2c1941294df3d4721c861677
Neptune <karl.davalleclercq@ynov.com>
2025-03-04 11:24:53 +0100

    correction db

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -2,20 +2,17 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Article;
 use App\Entity\Categories;
 use App\Entity\Author;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
-use Symfony\Component\Filesystem\Filesystem;
 
 class ArticleFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create('fr_FR');
-        $filesystem = new Filesystem();
 
         // Création d'un auteur par défaut
         $author = new Author();
@@ -24,7 +21,7 @@ class ArticleFixtures extends Fixture
 
         // Création de quelques catégories
         $categories = [];
-        $categoryNames = ['Meubles', 'Décoration', 'Électronique', 'Jouets', 'Livres'];
+        $categoryNames = ['JDR', 'Set de dés'];
 
         foreach ($categoryNames as $name) {
             $category = new Categories();
@@ -33,32 +30,7 @@ class ArticleFixtures extends Fixture
             $categories[] = $category;
         }
 
-        // Création de produits fictifs
-        for ($i = 1; $i <= 10; $i++) {
-            $article = new Article();
-            $productName = ucfirst($faker->words(2, true)); // Générer un nom
-            $article->setName($productName);
-            $article->setDescription($faker->sentence(10));
-            $article->setPrice($faker->randomFloat(2, 10, 500));
-            $article->setItemsStored($faker->numberBetween(1, 100));
-            $article->setPublicationDate($faker->dateTimeThisYear());
-            $article->setAuthor($author);
-            $article->setCategory($faker->randomElement($categories));
-
-            // Générer le nom du fichier image
-            $imageName = strtolower(str_replace([' ', "'", '"', ',', ';'], '_', $productName)) . '.png';
-            $imagePath = __DIR__ . '/../../public/assets/articleImages/' . $imageName;
-
-            // Vérifier si l'image existe
-            if ($filesystem->exists($imagePath)) {
-                $article->setCover($imageName);
-            } else {
-                $article->setCover('default.png'); // Image par défaut si non trouvée
-            }
-
-            $manager->persist($article);
-        }
-
+        // Exécution des requêtes en base de données
         $manager->flush();
     }
 }
